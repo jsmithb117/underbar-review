3e9cf1d1ae993f879bfc3e9d4eeffbd107385580
jasonschreib <jwschreiber1@gmail.com>
2020-10-16 13:00:13 -0700

    Fixed git problem/implemented up to reduce

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,13 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n === undefined) {
+      return array[array.length - 1];
+    } else if (n > array.length) {
+      return array.slice(0);
+    } else {
+      return array.slice(array.length - n);
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +53,20 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    //if is an array
+    if (Array.isArray(collection)) {
+      //iterate array (for loop)
+      for (let i = 0; i < collection.length; i++) {
+        //call iterator on element
+        iterator(collection[i], i, collection);
+      }
+    } else { //else
+      //iterate object (for in loop)
+      for (let elem in collection) {
+        //call iterator on element
+        iterator(collection[elem], elem, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,24 +88,66 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    //create output array
+    var output = [];
+    //iterate over the array (for loop)
+    for (var i = 0; i < collection.length; i++) {
+      //for each element call the test  //if the element passes
+      if (test(collection[i])) {
+        //push element to output array
+        output.push(collection[i]);
+      }
+    }
+    //return output array
+    return output;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    //create output array
+    var output = [];
+    //iterate over the array (for loop)
+    for (var i = 0; i < collection.length; i++) {
+      //for each element call the test  //if the element does not pass
+      if (!test(collection[i])) {
+        //push element to output array
+        output.push(collection[i]);
+      }
+    }
+    //return output array
+    return output;
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+    //create a result array []
+
+    //create an iterated array of calling iterator on each element [true, false, false, false, false]
+
+    //iterate iterated
+    //if !result.includes(array[i])
+    //if (iterate(array[i]) === iterated[i])
+    //push to result
+
+    //return the result array  [1,2]
   };
 
-
-  // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+
+    //create result array
+    var result = [];
+    //iterate over the collection
+    for (var i = 0; i < collection.length; i++) {
+      //apply the iterator to each element in the collection and push to result array
+      result.push(iterator(collection[i]));
+    }
+    //return result array
+    return result;
   };
 
   /*
@@ -107,24 +171,42 @@
   // Reduces an array or object to a single value by repetitively calling
   // iterator(accumulator, item) for each item. accumulator should be
   // the return value of the previous iterator call.
-  //  
+  //
   // You can pass in a starting value for the accumulator as the third argument
   // to reduce. If no starting value is passed, the first element is used as
   // the accumulator, and is never passed to the iterator. In other words, in
   // the case where a starting value is not passed, the iterator is not invoked
   // until the second element, with the first element as its second argument.
-  //  
+  //
   // Example:
   //   var numbers = [1,2,3];
   //   var sum = _.reduce(numbers, function(total, number){
   //     return total + number;
   //   }, 0); // should be 6
-  //  
+  //
   //   var identity = _.reduce([5], function(total, number){
   //     return total + number * number;
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    //array case
+    //
+    if (accumulator === undefined) {
+      //assign accumulator as collection[0]
+      accumulator = collection[0];
+      //iterate starting at i = 1
+      for (let i = 1; i < collection.length; i++) {
+        //apply iterator to that element and add it to accumulator
+        accumulator = iterator(accumulator, collection[i]);
+      }
+    } else { //else
+      //iterate starting at i = 0
+      for (let i = 0; i < collection.length; i++) {
+        //apply iterator to each element and add it to accumulator
+        accumulator = iterator(accumulator, collection[i]);
+      }
+    }
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
@@ -141,94 +223,10 @@
 
 
   // Determine whether all of the elements match a truth test.
-  _.every = function(collection, iterator) {
-    // TIP: Try re-using reduce() here.
-  };
+  // _*
+  //  * In this section, we'll look at a couple of helpers for merging objects.
+  // }*/
 
-  // Determine whether any of the elements pass a truth test. If no iterator is
-  // provided, provide a default one
-  _.some = function(collection, iterator) {
-    // TIP: There's a very clever way to re-use every() here.
-  };
-
-
-  /**
-   * OBJECTS
-   * =======
-   *
-   * In this section, we'll look at a couple of helpers for merging objects.
-   */
-
-  // Extend a given object with all the properties of the passed in
-  // object(s).
-  //
-  // Example:
-  //   var obj1 = {key1: "something"};
-  //   _.extend(obj1, {
-  //     key2: "something new",
-  //     key3: "something else new"
-  //   }, {
-  //     bla: "even more stuff"
-  //   }); // obj1 now contains key1, key2, key3 and bla
-  _.extend = function(obj) {
-  };
-
-  // Like extend, but doesn't ever overwrite a key that already
-  // exists in obj
-  _.defaults = function(obj) {
-  };
-
-
-  /**
-   * FUNCTIONS
-   * =========
-   *
-   * Now we're getting into function decorators, which take in any function
-   * and return out a new version of the function that works somewhat differently
-   */
-
-  // Return a function that can be called at most one time. Subsequent calls
-  // should return the previously returned value.
-  _.once = function(func) {
-    // TIP: These variables are stored in a "closure scope" (worth researching),
-    // so that they'll remain available to the newly-generated function every
-    // time it's called.
-    var alreadyCalled = false;
-    var result;
-
-    // TIP: We'll return a new function that delegates to the old one, but only
-    // if it hasn't been called before.
-    return function() {
-      if (!alreadyCalled) {
-        // TIP: .apply(this, arguments) is the standard way to pass on all of the
-        // infromation from one function call to another.
-        result = func.apply(this, arguments);
-        alreadyCalled = true;
-      }
-      // The new function always returns the originally computed result.
-      return result;
-    };
-  };
-
-  // Memorize an expensive function's results by storing them. You may assume
-  // that the function only takes primitives as arguments.
-  // memoize could be renamed to oncePerUniqueArgumentList; memoize does the
-  // same thing as once, but based on many sets of unique arguments.
-  //
-  // _.memoize should return a function that, when called, will check if it has
-  // already computed the result for the given argument and return that value
-  // instead if possible.
-  _.memoize = function(func) {
-  };
-
-  // Delays a function for the given number of milliseconds, and then calls
-  // it with the arguments supplied.
-  //
-  // The arguments for the original function are passed after the wait
-  // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
-  // call someFunction('a', 'b') after 500ms
-  _.delay = function(func, wait) {
-  };
 
 
   /**
